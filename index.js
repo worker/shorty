2c19309ac54e3906acd4d99373cc0a2c0e9a8391
worker <39812176+worker@users.noreply.github.com>
2023-10-17 19:49:21 +0000

    Loop through redirects

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ const redirects = YAML.parse(redirectsFile)
 console.log(redirects)
 
 // 2. Generate an html page for each redirect url from template.html
+const templateHTML = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8')
 
+// Loop through all url redirects, and generate an html page
+for (let [slug, url] of Object.entries(redirects)) {
+    console.log('Generating HTML Page for ', slug)
+    
+}
